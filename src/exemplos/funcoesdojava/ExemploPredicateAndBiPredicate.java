517b9dc37b7fa9135d4f70ca9d5b9bf321554c69
Luciano Ortiz <lucianoortizsilva@gmail.com>
2021-08-25 08:36:25 -0300

    Usando metodo default em Predicate<T>

--- src/exemplos/funcoesdojava/ExemploPredicateAndBiPredicate.java
+++ src/exemplos/funcoesdojava/ExemploPredicateAndBiPredicate.java
@@ -10,7 +10,18 @@ public class ExemploPredicateAndBiPredicate {
 		Predicate<String> p2 = x -> x.isEmpty();
 		System.out.println(p1.test(""));
 		System.out.println(p2.test(""));
-		
+
+		// Usando metodo default "AND"
+		Predicate<String> luciano = nome -> nome.contains("LUCIANO");
+		Predicate<String> ortiz = sobrenome -> sobrenome.contains("ORTIZ");
+		Predicate<String> nomeSobrenome = s -> s.contains("LUCIANO") && s.contains("ORTIZ");
+		Predicate<String> nomeSobrenomeMelhorado = luciano.and(ortiz);
+		Predicate<String> nomeSobrenomeOutro = luciano.and(ortiz.negate());
+		System.out.println("contains nomeSobrenome: " + nomeSobrenome.test("LUCIANOORTIZ"));
+		System.out.println("contains nomeSobrenomeMelhorado: " + nomeSobrenomeMelhorado.test("LUCIANO ORTIZ"));
+		System.out.println("contains nome e não sobrenome: " + nomeSobrenomeOutro.test("LUCIANO ORTIZ"));
+		////////////////////////////////////////////////////////////
+
 		BiPredicate<String, String> b1 = String::startsWith;
 		BiPredicate<String, String> b2 = (string, prefix) -> string.startsWith(prefix);
 		System.out.println(b1.test("Ortiz", "Outro"));
